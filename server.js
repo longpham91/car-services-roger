bc7a680e8a4357af81afd9d7d2d47cb0a9b07b3d
Long Pham <longpham91@gmail.com>
2016-03-07 17:12:30 -0500

    publish

---
+++ server.js
@@ -0,0 +1,26 @@
+var express      = require('express');
+var app          = express();
+//var favicon      = require('serve-favicon');
+var bodyParser   = require('body-parser');
+
+/**
+ * Routing & middlewares
+ */
+app.use(express.static(__dirname + '/static'));
+//app.use(favicon(__dirname + '/favicon.ico'));
+app.use(bodyParser.json({ extended: true }));
+
+app.use(function (request, response) {
+    response.status(404).json({
+        message: 'Oops! You may get lost!',
+        code: 'E_NOTFOUND'
+    });
+});
+
+app.use(function (error, request, response, next) {
+    response.status(500).json({
+        message: error.message,
+        code: error.code
+    });
+});
+module.exports = app;
