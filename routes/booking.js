538747b1b8483f1fafc866e5cfd98bc73b4a2241
Nghi <nghibui@chotot.vn>
2016-06-26 00:09:42 +0700

    fix booking form

--- routes/booking.js
+++ routes/booking.js
@@ -1,28 +1,33 @@
 var nodemailer = require('nodemailer');
 
 exports.post = function (request, response) {
-	if ((!request.body.name) || (!request.body.email) || (!request.body.message)) {
-		return response.status(500).json({ success: false, data: 'No arguments provided!'});
-	}
-
-	var smtpTransport = nodemailer.createTransport('smtps://uscar123%gmail.com:chuanchuan@smtp.gmail.com');
-	var name = request.body.first-name + request.body.last-name;
-	var email = request.body.email;
-    var phone = request.body.phone;
-	var message = request.body.message;
-    var tripType = request.body.trip-type;
-
-	var mailOptions = {
-		from: email, // sender address
-		to: 'uscar123@gmail.com', // reciever
-		subject: 'Website Contact Form: ' + name, // Subject line
-		html: 'You have received a new message from your website contact form.<br /><br />Here are the details:<br /><br /><strong>Name</strong>: ' + name + '<br /><br /><strong>Email</strong>: ' + email + '<br /><br /><strong>Message</strong>:<br />' + message
-	};
-
-	smtpTransport.sendMail(mailOptions, function (error, info) {
-		if (error) {
-			return response.status(500).json({ success: false, data: error});
+	try {
+		if (!request.body.email) {
+			return response.status(500).json({ success: false, data: 'No email provided!'});
 		}
-		response.json({result: 'Message sent: ' + info.response});
-	});
+
+		var smtpTransport = nodemailer.createTransport('smtps://uscar123%40gmail.com:chuanchuan@smtp.gmail.com');
+		var name = request.body.first - name + request.body.last - name;
+		var email = request.body.email;
+		var phone = request.body.phone;
+		var message = request.body.message;
+		// var tripType = request.body.trip - type;
+
+		var mailOptions = {
+			from: email, // sender address
+			to: 'uscar123@gmail.com', // reciever
+			subject: 'Website Contact Form: ' + name, // Subject line
+			html: 'You have received a new message from your website contact form.<br /><br />Here are the details:<br /><br /><strong>Name</strong>: ' + name + '<br /><br /><strong>Email</strong>: ' + email + '<br /><br /><strong>Message</strong>:<br />' + message
+		};
+		console.log('Sending mail');
+		smtpTransport.sendMail(mailOptions, function (error, info) {
+			if (error) {
+				console.log(error);
+				return response.status(500).json({ success: false, data: error});
+			}
+			response.json({result: 'Message sent: ' + info.response});
+		});
+	} catch(e) {
+		return response.status(500).json({ success: false, data: error});
+	}
 };
--- server.js
+++ server.js
@@ -1,39 +1,40 @@
-var express           = require('express');
-var router            = express.Router();
-var app               = express();
-//var favicon         = require('serve-favicon');
-var bodyParser        = require('body-parser');
+var express = require('express');
+var router = express.Router();
+var app = express();
+// var favicon         = require('serve-favicon');
+var bodyParser = require('body-parser');
 var BookingController = require('./routes/booking');
 
 /**
  * Routing & middlewares
  */
 app.use(express.static(__dirname + '/static'));
-//app.use(favicon(__dirname + '/favicon.ico'));
-app.use(bodyParser.json({ extended: true }));
+// app.use(favicon(__dirname + '/favicon.ico'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.use(router);
 
 router.post('/booking', BookingController.post);
 
 app.use(function (request, response) {
-    response.status(404).json({
-        message: 'Oops! You may get lost!',
-        code: 'E_NOTFOUND'
-    });
+	response.status(404).json({
+		message: 'Oops! You may get lost!',
+		code: 'E_NOTFOUND'
+	});
 });
 
 app.use(function (error, request, response, next) {
-    response.status(500).json({
-        message: error.message,
-        code: error.code
-    });
+	response.status(500).json({
+		message: error.message,
+		code: error.code
+	});
 });
 
 app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), () => {
-	console.log('Foodee service runs on port: ' + app.get('port'));
+	console.log('Car services runs on port: ' + app.get('port'));
 });
 
 module.exports = app;
